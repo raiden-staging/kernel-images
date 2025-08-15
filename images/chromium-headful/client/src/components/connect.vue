25a6fe623e47d29d5f4b6440662f34ba0b63107e
raiden-staging <staging@raiden.ai>
2025-08-15 19:10:06 +0100

    live web client [loading spinner update ✿]

--- images/chromium-headful/client/src/components/connect.vue
+++ images/chromium-headful/client/src/components/connect.vue
@@ -11,8 +11,7 @@
         </button>
       </form>
       <div class="loader" v-if="connecting">
-        <div class="bounce1"></div>
-        <div class="bounce2"></div>
+        <img src="/kernel.svg" class="spinning-logo" alt="Loading..." />
       </div>
     </div>
   </div>
@@ -103,40 +102,25 @@
       .loader {
         width: 90px;
         height: 90px;
-        position: relative;
         margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-        .bounce1,
-        .bounce2 {
+        .spinning-logo {
           width: 100%;
           height: 100%;
-          border-radius: 50%;
-          background-color: $style-primary;
-          opacity: 0.6;
-          position: absolute;
-          top: 0;
-          left: 0;
-
-          -webkit-animation: bounce 2s infinite ease-in-out;
-          animation: bounce 2s infinite ease-in-out;
-        }
-
-        .bounce2 {
-          -webkit-animation-delay: -1s;
-          animation-delay: -1s;
+          animation: spin 2s linear infinite;
         }
       }
     }
 
-    @keyframes bounce {
-      0%,
-      100% {
-        transform: scale(0);
-        -webkit-transform: scale(0);
+    @keyframes spin {
+      from {
+        transform: rotate(0deg);
       }
-      50% {
-        transform: scale(1);
-        -webkit-transform: scale(1);
+      to {
+        transform: rotate(360deg);
       }
     }
   }
