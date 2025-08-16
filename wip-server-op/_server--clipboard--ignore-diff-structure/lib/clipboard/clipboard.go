bfc9f0a041605f8d8acd31aa63c4b2ab8eb1392f
raiden-staging <staging@raiden.ai>
2025-08-16 03:30:21 +0000

    clipboard :check_emoji: *

--- wip-server-op/_server--clipboard--ignore-diff-structure/lib/clipboard/clipboard.go
+++
@@ -1,4 +0,0 @@
-package clipboard
-
-// This file is intentionally left empty to avoid type conflicts
-// The clipboard implementation is defined in the cmd/api/api/clipboard.go file
--- wip-server-op/_server--clipboard--ignore-diff-structure/lib/clipboard/types.go
+++
@@ -1,4 +0,0 @@
-package clipboard
-
-// This file is intentionally left empty to avoid type conflicts
-// The clipboard types are defined in the cmd/api/api/clipboard.go file
