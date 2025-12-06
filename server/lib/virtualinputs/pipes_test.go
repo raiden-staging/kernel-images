19cbf6d7213954b1e1f33acc1b66b7ab467f6129
raiden-staging <staging@raiden.ai>
2025-12-06 13:29:59 +0000

    [mediav1@rev2] add test ensuring OpenPipeReadWriter does not block when no peer

--- server/lib/virtualinputs/pipes_test.go
+++ server/lib/virtualinputs/pipes_test.go
@@ -39,3 +39,16 @@ func TestOpenPipeWriterTimesOutWithoutReader(t *testing.T) {
 	require.Error(t, err)
 	require.GreaterOrEqual(t, time.Since(start), 180*time.Millisecond)
 }
+
+func TestOpenPipeReadWriterDoesNotBlockWithoutPeer(t *testing.T) {
+	t.Parallel()
+
+	dir := t.TempDir()
+	pipe := filepath.Join(dir, "keepalive.pipe")
+	require.NoError(t, syscall.Mkfifo(pipe, 0o666))
+
+	f, err := OpenPipeReadWriter(pipe, time.Second)
+	require.NoError(t, err)
+	require.NotNil(t, f)
+	require.NoError(t, f.Close())
+}
